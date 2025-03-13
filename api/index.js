e64ada7218677828812fbb4a3dc60f839083b317
Fousiya <fouziyakashmir83@gmail.com>
2025-03-14 00:01:03 +0530

    created signup UI

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './Routes/userRoute.js'
+import authRoute from './Routes/authRoute.js'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -10,10 +11,22 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 const app = express()
+app.use(express.json())
 
 app.listen(3000,()=>{
     console.log('Server listening on port 3000')
 })
 
 app.use('/api/user',userRoute)
+app.use('/api/auth',authRoute)
+
+app.use((err,req,res,next)=>{
+    const statusCode= err.statusCode || 500
+    const message = err.message ||'Internal server error'
+    return res.status(statusCode.json({
+        success:false,
+        message,
+        statusCode
+    }))
+})
 
